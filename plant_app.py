8958c8df8de379c283512ba635f0060a723ad9c5
jahnvimalviya12-coder <jahnvimalviya12@gmail.com>
2026-07-11 09:58:59 +0530

    Update plant_app.py

--- plant_app.py
+++ plant_app.py
@@ -5,7 +5,7 @@ from PIL import Image
 import pickle
 
 # Load model
-model = tf.keras.models.load_model("plant_disease_model_new.h5")
+model = tf.keras.models.load_model("plant_disease_model.h5")
 
 # Load class names
 with open("class_names.pkl", "rb") as f:
@@ -20,7 +20,6 @@ uploaded_file = st.file_uploader(
     "Choose a leaf image",
     type=["jpg", "jpeg", "png"]
 )
-
 if uploaded_file is not None:
 
     image = Image.open(uploaded_file).convert("RGB")
@@ -37,7 +36,17 @@ if uploaded_file is not None:
     prediction = model.predict(img)
 
     predicted_index = np.argmax(prediction)
+
+    st.write("Prediction Shape:", prediction.shape)
+    st.write("Predicted Index:", predicted_index)
+    st.write("Total Class Names:", len(class_names))
+
     confidence = np.max(prediction) * 100
 
-    st.success(f"Predicted Disease: {class_names[predicted_index]}")
-    st.info(f"Confidence: {confidence:.2f}%")
+    if predicted_index < len(class_names):
+        st.success(f"Predicted Disease: {class_names[predicted_index]}")
+        st.info(f"Confidence: {confidence:.2f}%")
+    else:
+        st.error("Prediction index is out of range.")
+
+
