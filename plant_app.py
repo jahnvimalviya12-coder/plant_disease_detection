bb72eb9eff17149e1d45f8425ed17b5cb36c2a85
jahnvimalviya12-coder <jahnvimalviya12@gmail.com>
2026-07-11 10:57:16 +0530

    Update plant_app.py

--- plant_app.py
+++ plant_app.py
@@ -4,48 +4,64 @@ import numpy as np
 from PIL import Image
 import pickle
 
-# Load model
+# Page settings
+st.set_page_config(
+    page_title="Plant Disease Detection",
+    page_icon="🌿"
+)
+
+# Load trained model
 model = tf.keras.models.load_model("plant_disease_model_new.h5")
 
 # Load class names
 with open("class_names.pkl", "rb") as f:
     class_names = pickle.load(f)
-st.write(type(class_names))
-st.write(len(class_names))
-st.write(class_names)
 
-st.set_page_config(page_title="Plant Disease Detection", page_icon="🌿")
 
+# Title
 st.title("🌿 Plant Disease Detection")
 st.write("Upload a plant leaf image to detect its disease.")
 
+
+# Upload image
 uploaded_file = st.file_uploader(
     "Choose a leaf image",
     type=["jpg", "jpeg", "png"]
 )
+
+
 if uploaded_file is not None:
 
+    # Read image
     image = Image.open(uploaded_file).convert("RGB")
 
-    st.image(image, caption="Uploaded Leaf Image", use_container_width=True)
+    # Display smaller image
+    st.image(
+        image,
+        caption="Uploaded Leaf Image",
+        width=300
+    )
 
-    # Preprocess image
-    img = image.resize((64,64))
+
+    # Preprocess image for model (128x128)
+    img = image.resize((128, 128))
+
     img = np.array(img)
+
+    # Normalize pixel values
     img = img / 255.0
+
+    # Add batch dimension
     img = np.expand_dims(img, axis=0)
+
 
     # Prediction
     prediction = model.predict(img)
 
     predicted_index = np.argmax(prediction)
 
-   
 
-    if predicted_index < len(class_names):
-        st.success(f"Predicted Disease: {class_names[predicted_index]}")
-       
-    else:
-        st.error("Prediction index is out of range.")
-
-
+    # Display result
+    st.success(
+        f"🌿 Predicted Disease: {class_names[predicted_index]}"
+    )
